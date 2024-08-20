912418abef986f0df77ae198794c4a5bf86af931
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-20 09:33:10 +0200

    Core passed tests

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,21 +3,30 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
+
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> preInstalled) {
+        this.installedGames = preInstalled;
+        this.psu = psu;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        this.psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String gameName) {
+        this.installedGames.add(new Game(gameName));
     }
 
-    public String playGame() {
+    public String playGame(String game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(game)) {
                 return g.start();
             }
         }
